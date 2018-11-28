cf3750631cdd6fd91fb8aab3a60bc232b11e1b61
Simon Cannon <simon.cannon@gmail.com>
2018-11-28 13:35:37 +0000

    Initial commit

---
+++ Chopext461/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chopext461.MvcApplication" Language="C#" %>
---
+++ Chopext461/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chopext461.Startup))]
+namespace Chopext461
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
